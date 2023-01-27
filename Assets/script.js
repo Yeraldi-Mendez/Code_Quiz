90fc1c963e4f2f9417ec1811a6bbf14b28c5d286
Yeraldi-Mendez <yeraldimendez.ym@gmail.com>
2023-01-27 00:00:10 -0800

    added last funcgions into js, made changes to html in spelling, and changed styling is css. Code not function as desire

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,9 @@
-/* got help code from website: simplestepscose.com/javascript-quiz-tutorial 
-on how to put questions in*/
+/* got help code to put questions into js from 
+website: simplestepscose.com/javascript-quiz-tutorial 
+*/
 /* got quiz questions from https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS*/
 
-var myQuestions = [
+const myQuestions = [
     {
         question: "Inside which HTML element do we put the JavaScript?",
         answers: {
@@ -60,7 +61,7 @@ var myQuestions = [
 ];
 
 //declaring variables
-var start = document.getElementById("start-quiz");
+var startQuiz = document.getElementById("start-quiz");
 var questions = document.getElementById("questions");
 var answers = document.getElementById("answers");
 var timer = document.getElementById("countdown");
@@ -68,33 +69,118 @@ var score = document.getElementById("score");
 var initials = document.getElementById("initials");
 var submit = document.getElementById("submit");
 var highscore = document.getElementById("highscore");
-var activeQuestion = 1;
+var activeIndex = 1;
 var timeLeft = 60;
 var games = [];
 
 
 //this is event listener to start quiz/timer
-start.addEventListener("click", function(){
+startQuiz.addEventListener("click", function () {
     countdown();
     renderQuestions(myQuestions[0]);
-});
-
-questions.addEventListener("click", function(event){
-    if (activeQuestion === 5) {
-        answers.textContent = "Congrats! All correct, Game over";
+  });
+  
+//noticed timer goes by too quick
+// needs to be resolved
+questions.addEventListener("click", function (event) {
+    if (activeIndex === 5) {
+        answers.textContent = "Congrats! Game over";
         score.textContent = timeLeft;
     }
 
-    else if(event.target.textContent !== myQuestions[activeQuestion - 1].correctAnswer)
+    else if(event.target.textContent !== myQuestions[activeIndex - 1].correctAnswer)
     {
         answers.textContent = "Opps, try again!";
-        timeLeft -= 5;
+        timeLeft -= 10;
     }
 
     else {
         answers.textContent = "You are correct!";
         console.log(timeLeft);
-        activeQuestion++;
-        renderQuestions(myQuestions[activeQuestion - 1]);
+        activeIndex++;
+        renderQuestions(myQuestions[activeIndex - 1]);
     }
 });
+
+//this will help display questions and start quiz
+
+function renderQuestions(activeQuestion){
+    questions.innerHTML = "";
+
+    //adds question and answer el to html
+    var qTitle = document.createElement("p");
+    var answerList = document.createElement("ol");
+    var answer1 = document.createElement ("li");
+    var answer2 = document.createElement ("li");
+    var answer3 = document.createElement ("li");
+    var answer4 = document.createElement ("li");
+
+    qTitle.textContent = activeQuestion.question;
+    answer1.textContent = activeQuestion.answers[1];
+    answer2.textContent = activeQuestion.answers[2];
+    answer3.textContent = activeQuestion.answers[3];
+    answer4.textContent = activeQuestion.answers[4];
+
+    answerList.append(answer1);
+    answerList.append(answer2);
+    answerList.append(answer3);
+    answerList.append(answer4);
+    questions.append(qTitle);
+    questions.append(answerList);
+}
+
+//timer from 60
+function countdown(){
+    var timeIntervals = setInterval(function(){
+        if (timeLeft > 1 && activeIndex !== 5){
+            timer.textContent = timeLeft;
+            timeLeft--;
+        }
+        else{
+            clearInterval(timeIntervals);
+        }
+    }, 1000);
+}
+
+//buttons
+//submit but clicks but does not show name
+submit.addEventListener("click", function(event){
+    event.preventDefault();
+    var games = {
+        initials: initials.value.trim(),
+        score: timeLeft //this needs to score of how many questions they got rihgt
+    };
+    games.push(game);
+    storeGames();
+    renderGames();
+});
+
+//I could not get highscore to show up
+//need to resolve
+
+function renderGames(){
+    for(var i = 0; i < games.length; i++){
+        console.log(games[i]);
+        var hScore = document.createElement("li");
+        hScore.textContent = games[i].initials + " " + games[i].score;
+        highscore.append(hScore);
+    }
+
+    console.log(games.length);
+}
+
+//I believe that we learned this for this hw but I cannot remember (JSON)
+function storeGames(){
+    console.log(games);
+    localStorage.setItem("games",JSON.stringify(games));
+}
+
+//funstion will load when page loads
+function init(){
+    var storeGames = JSON.parse (localStorage.getItem("games"));
+    if(storeGames !== null){
+        games = storeGames;
+    }
+}
+
+init();
